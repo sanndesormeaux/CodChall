35dcecfb0496161a791579ee1e0add9e2d7d8ebf
Sann <sony973@live.fr>
2019-12-13 18:05:24 +0100

    zafzafaz

--- js/script.js
+++ js/script.js
@@ -41,4 +41,55 @@ var valeur = 0;
             document.getElementById('nombre').innerHTML = valeur;
         }
 
-7
+        var x = window.innerWidth;
+        var y = window.innerHeight;
+        var divwidth = 0;
+        var divheight = 0;
+    function taille() {
+		document.getElementById('width').innerHTML = x;
+		document.getElementById('height').innerHTML = y;
+        divwidth = x/2;
+        divheight = y/4;
+        document.getElementById('divwidth').innerHTML = divwidth;
+		document.getElementById('divheight').innerHTML = divheight;
+    }
+
+    var phrase = [];
+var mot = document.getElementById("mot").value;
+
+    function ajouter() {
+        var new1 = phrase.push([mot]);
+        displayList();
+    }
+
+    function retirer(argument) {
+        var new2 = phrase.splice(argument, 1);
+        displayList();
+    }
+
+    function displayList() {
+        laphrase.innerHTML = "";
+        for (i=0; i<phrase.length; i++) {
+            laphrase.innerHTML += phrase[i];
+        }
+    }
+
+    var mcube = document.getElementById('mcube');
+    var conv1 = 0;
+    var conv2 = 0;
+    
+        function conversion1() {
+            conv1 = mcube.value/1000;
+            document.getElementById('conversion1').innerHTML = conv1+"L";
+        }
+    
+        function conversion2() {
+            conv2 = mcube.value*1000;
+            document.getElementById('conversion2').innerHTML = conv2+"mc3";
+        }
+
+        var nbdiv = document.getElementById('nbdiv');
+	function div() {
+        var result = nbdiv.value/2;
+		document.getElementById('result').innerHTML = result;
+	}
